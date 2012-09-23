4cd29d90a9a0f22ebb1668ac8cffb9ab21d6160d
じゅんいち☆かとう <j5ik2o@gmail.com>
2012-09-24 03:09:39 +0900

    refactor & addition license

---
+++ src/test/scala/org/sisioh/sip/message/header/impl/FromDecoderSpec.scala
@@ -0,0 +1,12 @@
+package org.sisioh.sip.message.header.impl
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: junichi
+ * Date: 2012/09/24
+ * Time: 0:35
+ * To change this template use File | Settings | File Templates.
+ */
+class FromDecoderSpec {
+
+}
