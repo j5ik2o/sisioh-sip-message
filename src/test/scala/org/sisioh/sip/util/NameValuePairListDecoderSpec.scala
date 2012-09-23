41d4051ac811398a75943468803fdbf9394755cc
じゅんいち☆かとう <j5ik2o@gmail.com>
2012-09-23 13:37:34 +0900

    refactor

---
+++ src/test/scala/org/sisioh/sip/util/NameValuePairListDecoderSpec.scala
@@ -0,0 +1,12 @@
+package org.sisioh.sip.util
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: junichi
+ * Date: 2012/09/23
+ * Time: 11:51
+ * To change this template use File | Settings | File Templates.
+ */
+class NameValuePairListDecoderSpec {
+
+}
---
+++ src/test/scala/org/sisioh/sip/util/ParserBaseSpec.scala
@@ -0,0 +1,12 @@
+package org.sisioh.sip.util
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: junichi
+ * Date: 2012/09/22
+ * Time: 21:58
+ * To change this template use File | Settings | File Templates.
+ */
+class ParserBaseSpec {
+
+}
