2c4147ecdca1d824117fd3e1f88fa00b8a83d0b4
じゅんいち☆かとう <j5ik2o@gmail.com>
2012-09-24 00:21:56 +0900

    refactor

---
+++ src/test/scala/org/sisioh/sip/message/header/impl/ToDecoderSpec.scala
@@ -0,0 +1,12 @@
+package org.sisioh.sip.message.header.impl
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: junichi
+ * Date: 2012/09/23
+ * Time: 15:04
+ * To change this template use File | Settings | File Templates.
+ */
+class ToDecoderSpec {
+
+}
